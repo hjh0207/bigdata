145521bf0d3db674a9506ffed3ff3ed19b1b7cf9
hjh0207 a <h2j10207@gmail.com>
2023-03-27 07:15:22 +0000

    평점 애매율 개봉날짜 추가

--- Dmovie.py
+++ Dmovie.py
@@ -8,7 +8,14 @@ soup = BeautifulSoup(data.text, 'html.parser')
 
 movielist = soup.select(".tit_item")
 rank = 0
-for tr in movielist:
-    a_tag = tr.select_one("a")	
+movie_list = soup.select(".thumb_cont")
+for tr in movie_list:
     rank = rank + 1	
+    a_tag = tr.select_one("a")	
     print(f'{rank}위 {a_tag.text}')	
+    txt_grade = tr.select_one("span.txt_grade")	
+    print(f'평점 : {txt_grade.text}')
+    txt_num = tr.select_one("span.txt_num")
+    print(f'예매율 : {txt_num.text}')
+    txt_date = tr.select_one(".txt_info > span.txt_num")
+    print(f'개봉날짜 : {txt_date.text}')
